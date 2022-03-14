fbf688e85442184136c88280e8f9e129d04967f4
Fabi <naibaf.to@gmail.com>
2022-03-14 19:40:14 +0100

    add ignore and try sth

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import tensorflow as tf
-import keras
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_addons as tfa
 
-name = Fabi
+from kaggledatasets import KaggleDatasets
+import matplotlib.pyplot as plt
+import numpy as np
+
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    print('Device:', tpu.master())
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+except:
+    strategy = tf.distribute.get_strategy()
+print('Number of replicas:', strategy.num_replicas_in_sync)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+print(tf.__version__)
+
+GCS_PATH = kaggledatasets().get_gcs_path()
